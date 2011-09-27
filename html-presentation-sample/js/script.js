52fcf36bec0b6e380edfd23c05dfa82617aa770f
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2011-09-27 19:10:32 +0200

    Improved presentation with new fonts.

--- html-presentation-sample/js/script.js
+++ html-presentation-sample/js/script.js
@@ -133,13 +133,35 @@ $(document).ready(function() {
 	//keyboard navigation
 	$(document).bind('keydown',function(e) {
 		switch(e.keyCode) {
+			case 8: //backspace
+				e.preventDefault();
+				pageTurn('previous');
+				break;
+			case 13: //enter
+				e.preventDefault();
+				pageTurn('next');
+				break;
 			case 32: //space
 				e.preventDefault();
 				pageTurn('next');
 				break;
+			case 33: //page up
+				e.preventDefault();
+				pageTurn('previous');
+				break;
+			case 34: //page down
+				e.preventDefault();
+				pageTurn('next');
+				break;
+			case 37: //left
+				// nothing for now
+				break;
 			case 38: //up
 				e.preventDefault();
 				pageTurn('previous');
+				break;
+			case 39: //right
+				// nothing for now
 				break;
 			case 40: //down
 				e.preventDefault();
@@ -148,4 +170,57 @@ $(document).ready(function() {
 		}
 	});
 	
+	
+	//scrolling
+	$(window).scroll(function (e) {
+		//when the top of the next slide is at 80% of the top of the screen, we will animate the next slide content.  
+		var top = $(document).scrollTop();
+		var windowHeight = Math.max(800,$(window).height());
+		
+		if (top > windowHeight * 0.8 && top < windowHeight) {
+			//scrolling to page 2
+			$("#back-to-top").css({"left": "0", "top": "0"});
+			$("#acceleo-logo").css({"-webkit-transform" : "rotate(-58deg)", "-webkit-transition-duration": "2s"});
+		} else if (top > windowHeight && top < 2 * windowHeight) {
+			//scrolling to page 3
+			$("#back-to-top").css({"left": "50", "top": "0"});
+		} else if (top > 2 * windowHeight && top < 3 * windowHeight) {
+			//scrolling to page 4
+			$("#back-to-top").css({"left": "50", "top": "200"});
+		} else if (top > 3 * windowHeight && top < 4 * windowHeight) {
+			//scrolling to page 5
+		} else if (top > 4 * windowHeight && top < 5 * windowHeight) {
+			//scrolling to page 6
+		} else if (top > 5 * windowHeight && top < 6 * windowHeight) {
+			//scrolling to page 7
+		} else if (top > 6 * windowHeight && top < 7 * windowHeight) {
+			//scrolling to page 8
+		} else if (top > 7 * windowHeight && top < 8 * windowHeight) {
+			//scrolling to page 9
+		} else if (top > 8 * windowHeight && top < 9 * windowHeight) {
+			//scrolling to page 10
+		} else if (top > 9 * windowHeight && top < 10 * windowHeight) {
+			//scrolling to page 11
+		} else if (top > 10 * windowHeight && top < 11 * windowHeight) {
+			//scrolling to page 12
+		} else if (top > 11 * windowHeight && top < 12 * windowHeight) {
+			//scrolling to page 13
+		} else if (top > 12 * windowHeight && top < 13 * windowHeight) {
+			//scrolling to page 14
+		} else if (top > 13 * windowHeight && top < 14 * windowHeight) {
+			//scrolling to page 15
+		} else if (top > 14 * windowHeight && top < 15 * windowHeight) {
+			//scrolling to page 16
+		} else if (top > 15 * windowHeight && top < 16 * windowHeight) {
+			//scrolling to page 17
+		} else if (top > 16 * windowHeight && top < 17 * windowHeight) {
+			//scrolling to page 18
+		} else if (top > 17 * windowHeight && top < 18 * windowHeight) {
+			//scrolling to page 19
+		} else if (top > 18 * windowHeight && top < 19 * windowHeight) {
+			//scrolling to page 20
+		} else if (top > 19 * windowHeight && top < 20 * windowHeight) {
+			//scrolling to page 21
+		}
+	});
 });
